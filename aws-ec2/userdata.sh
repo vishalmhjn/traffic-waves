17dd524c3888851c96e1e29563bfe9f7d636c867
Vishal <vishalmhjn3@gmail.com>
2024-02-28 09:26:12 +0100

    add: aws lambda functions and ec2 user data

---
+++ aws-ec2/userdata.sh
@@ -0,0 +1,29 @@
+Content-Type: multipart/mixed; boundary="//"
+MIME-Version: 1.0
+
+--//
+Content-Type: text/cloud-config; charset="us-ascii"
+MIME-Version: 1.0
+Content-Transfer-Encoding: 7bit
+Content-Disposition: attachment; filename="cloud-config.txt"
+
+#cloud-config
+cloud_final_modules:
+- [scripts-user, always]
+
+--//
+Content-Type: text/x-shellscript; charset="us-ascii"
+MIME-Version: 1.0
+Content-Transfer-Encoding: 7bit
+Content-Disposition: attachment; filename="userdata.txt"
+
+#!/bin/bash
+cd /home/ec2-user/parisTraffic/
+source venv/bin/activate
+cd src/
+python call_api.py
+rm /tmp/response_data*.csv
+python preprocess_data.py
+python inference.py
+python app.py
+--//
